b0682aa1deb8091687bda0dcfe8c43d38711becb
JaeHyuk Choi <jaehyuk1220@gmail.com>
2022-08-30 11:16:56 +0900

    실로폰 구현

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,74 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: TextButton(
-            child: Container(color: Colors.red),
-            onPressed: () {
-              var player = AudioPlayer();
-              player.play(AssetSource('note1.wav'));
-            },
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.red),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note1.wav'));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.orange),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note2.wav'));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.yellow),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note3.wav'));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.green),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note4.wav'));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.blue),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note5.wav'));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.blue.shade900),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note6.wav'));
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: TextButton(
+                    child: Container(color: Colors.purple),
+                    onPressed: () {
+                      var player = AudioPlayer();
+                      player.play(AssetSource('note7.wav'));
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
